39477d7df5265fa2d8bcf5353c199a1b5aeb0182
vac <vac@milowice.net>
2015-03-05 22:29:47 +0100

    OpenGLHostedControl control for WPF - a nativewindow based control. it does not need to fill whole window. it's over 10x faster that standard wpf control.

---
+++ source/SharpGL/Core/SharpGL.WPF/GLHwndHost.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+
+namespace SharpGL.WPF
+{
+    public class GLHwndHost : HwndHost
+    {
+        public delegate void AfterSizeChangedHandler();
+
+        internal const int
+            WS_CHILD = 0x40000000,
+            WS_VISIBLE = 0x10000000,
+            LBS_NOTIFY = 0x00000001,
+            HOST_ID = 0x00000002,
+            LISTBOX_ID = 0x00000001,
+            WS_VSCROLL = 0x00200000,
+            WS_BORDER = 0x00800000;
+
+        private IntPtr hwndHost;
+
+        [DllImport("user32.dll", EntryPoint = "DestroyWindow", CharSet = CharSet.Unicode)]
+        internal static extern bool DestroyWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll", EntryPoint = "CreateWindowEx", CharSet = CharSet.Unicode)]
+        internal static extern IntPtr CreateWindowEx(int dwExStyle,
+            string lpszClassName,
+            string lpszWindowName,
+            int style,
+            int x, int y,
+            int width, int height,
+            IntPtr hwndParent,
+            IntPtr hMenu,
+            IntPtr hInst,
+            [MarshalAs(UnmanagedType.AsAny)] object pvParam);
+
+        protected override IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            handled = false;
+            return IntPtr.Zero;
+        }
+
+        protected override void DestroyWindowCore(HandleRef hwnd)
+        {
+            DestroyWindow(hwnd.Handle);
+        }
+
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            base.OnRenderSizeChanged(sizeInfo);
+            if (AfterSizeChanged != null)
+            {
+                Dispatcher.BeginInvoke(
+                    DispatcherPriority.Normal,
+                    new Action(() => AfterSizeChanged()));
+            }
+        }
+
+        public event AfterSizeChangedHandler AfterSizeChanged;
+
+        protected override HandleRef BuildWindowCore(HandleRef hwndParent)
+        {
+            hwndHost = CreateWindowEx(0, "static", "",
+                WS_CHILD,
+                0, 0,
+                100, 100,
+                hwndParent.Handle,
+                (IntPtr)HOST_ID,
+                IntPtr.Zero,
+                0);
+
+
+            return new HandleRef(this, hwndHost);
+        }
+    }
+}
